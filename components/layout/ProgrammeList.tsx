9b32423a2d0f30dc7b5c9b6e0dad9f7e0c0d1ed6
Tebalo <btebalo@gmail.com>
2023-04-21 12:27:03 +0200

    wip

--- components/layout/ProgrammeList.tsx
+++ components/layout/ProgrammeList.tsx
@@ -13,19 +13,19 @@ const ProgrammeList = () => {
         <div className='content-container'>
             <TopNavigation />
             <div className='h-screen flex flex-col bg-white dark:bg-gray-900 mx-1 my-1'>
-                    <section className='bg-gray-200 my-5 mx-5 shrink-0'>
-                        <div className='mx-5 my-3'>
-                            <RecommendedCourses/>
-                        </div>
-                    </section>
+                <section className='bg-gray-300 my-5 mx-5 shrink-0'>
+                    <div className='mx-5 my-3'>
+                        <RecommendedCourses />
+                    </div>
+                </section>
                 <div className='mx-5 grid grid-cols-2 gap-5'>
                     <section className='bg-gray-200 h-80 my-5 bg-gray-200 px-4 py-2'>
                         <div className='text-lg font-medium text-gray-800 mb-2'>Scholarships</div>
-                        <Divider/>
+                        <Divider />
                     </section>
                     <section className='bg-gray-200 h-80 my-5 bg-gray-200 px-4 py-2'>
                         <div className='text-lg font-medium text-gray-800 mb-2'>Top Offers</div>
-                        <Divider/>
+                        <Divider />
                     </section>
                 </div>
             </div>
@@ -34,12 +34,13 @@ const ProgrammeList = () => {
 };
 
 
-const Label = ({label,value}:{label:string,value:string}) =>{
-return(
-<div className='flex items-center mb-4'>
-    <div className='w-1/3 font-medium text-gray-600'>{label}:</div>
-    <div className='w-2/3 text-gray-800'>{value}</div> 
-</div>)};
-const Divider = () => <hr className='border-t-2 border-gray-300 mb-2'/>;
+const Label = ({ label, value }: { label: string, value: string }) => {
+    return (
+        <div className='flex items-center mb-4'>
+            <div className='w-1/3 font-medium text-gray-600'>{label}:</div>
+            <div className='w-2/3 text-gray-800'>{value}</div>
+        </div>)
+};
+const Divider = () => <hr className='border-t-2 border-gray-300 mb-2' />;
 
 export default ProgrammeList;
