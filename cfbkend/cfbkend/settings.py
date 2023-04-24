7ddd6ebcf4c213e70b998d29064a8efdb422e25e
Tebalo <btebalo@gmail.com>
2023-04-25 01:12:15 +0200

    connected to MySQL DB

--- cfbkend/cfbkend/settings.py
+++ cfbkend/cfbkend/settings.py
@@ -120,11 +120,22 @@ WSGI_APPLICATION = 'cfbkend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cfb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
