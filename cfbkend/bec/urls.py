c8254a37f2dd93679c9d91f17f29a1c3696433d7
Tebalo <btebalo@gmail.com>
2023-04-27 11:30:31 +0200

    backend in progress

--- cfbkend/bec/urls.py
+++ cfbkend/bec/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CandidateList, CandidateDetail, candidate_list
+from .views import CandidateList, CandidateDetail, candidate_list, candidate
 
 urlpatterns = [
     path('candidates/', CandidateList.as_view()),
     path('candidate-list/', candidate_list, name='candidate-list'),
     path('candidates/<int:pk>/', CandidateDetail.as_view()),
+    path('candidate/',candidate, name='candidate'),
 ]
--- cfbkend/bec/views.py
+++ cfbkend/bec/views.py
@@ -25,3 +25,18 @@ def candidate_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET'])
+def candidate(request):
+    if request.method == "GET":
+        candidate_id = request.data['candidate_id']
+        center_number = request.data['center_number']
+
+        # Retrieve the candidate object from the database
+        try:
+            candidate = Candidate.objects.get(candidate_id=candidate_id, center_number=center_number)
+        except Candidate.DoesNotExist:
+            return Response({'error': 'Candidate not found'},status=status.HTTP_404_NOT_FOUND)
+        
+        # Serialize the candidate object and return as response
+        serializer = CandidateSerializer(candidate)
+        return Response(serializer.data)
