ea0c64f9845b99502708cfe154364a4384fa3cd5
Tebalo <btebalo@gmail.com>
2023-05-02 01:37:22 +0200

    Initial creation of cookies

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 require('text-encoder');
+import Cookies from 'js-cookie';
 
 const LoginPage = () => {
 
